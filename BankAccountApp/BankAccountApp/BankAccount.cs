faab418bb402119114f0bf08fba986815a913fcc
Dexter Brings <dexter.brings@gmail.com>
2025-02-16 16:16:00 +0100

    initial commit

---
+++ BankAccountApp/BankAccountApp/BankAccount.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountApp
+{
+    class BankAccount
+    {
+        public int Number { get; private set; }
+
+        public string Owner { get; private set; }
+
+        public float Balance { get; private set; }
+
+
+        public BankAccount(int number, string owner, float initialBalance)
+        {
+            Number = number;
+            Owner = owner;
+            Balance = initialBalance;
+        }
+
+        public void MakeDeposit(float amount)
+        {
+            Balance += amount;
+        }
+
+        public void MakeWithdrawal(float amount)
+        {
+            if(amount <= Balance)
+            {
+                Balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Kontostand reicht nicht aus. Bitte anderen Betrag eingeben.");
+            }
+        }
+
+        public void PrintBalance()
+        {
+            Console.WriteLine($"Der Kontostand vom Konto {Owner} beträgt: {Balance} Euro");
+        }
+
+    }
+}
